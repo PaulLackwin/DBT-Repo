05fcb9020bddad3109921f1839d8fe1027df51b0
paullackwin2000@gmail.com <paullackwin2000@gmail.com>
2024-03-11 11:25:39 +0000

    Product revenue generation

---
+++ models/intermediate/int_product_revenue.sql
@@ -0,0 +1,17 @@
+WITH transaction_revenue AS (
+  SELECT
+    t.PRODUCTID,
+    SUM(t.TOTALAMOUNT) AS total_revenue
+  FROM
+  {{ ref('stg_transaction') }} t
+  GROUP BY
+    t.PRODUCTID
+)
+
+SELECT
+  p.*,
+  tr.total_revenue
+FROM
+  {{ ref('stg_product') }} p
+LEFT JOIN
+  transaction_revenue tr ON p.PRODUCTID = tr.PRODUCTID
