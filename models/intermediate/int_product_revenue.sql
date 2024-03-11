5cfc87b52a37bd75f5a018f879a4633402df8fbd
paullackwin2000@gmail.com <paullackwin2000@gmail.com>
2024-03-11 12:19:13 +0000

    Bug Fix

--- models/intermediate/int_product_revenue.sql
+++ models/intermediate/int_product_revenue.sql
@@ -3,15 +3,19 @@ WITH transaction_revenue AS (
     t.PRODUCTID,
     SUM(t.TOTALAMOUNT) AS total_revenue
   FROM
-  {{ ref('stg_transaction') }} t
+    {{ ref('stg_transaction') }} t
   GROUP BY
     t.PRODUCTID
 )
 
 SELECT
   p.*,
-  tr.total_revenue
+  tr.total_revenue 
 FROM
   {{ ref('stg_product') }} p
 LEFT JOIN
   transaction_revenue tr ON p.PRODUCTID = tr.PRODUCTID
+WHERE
+  tr.total_revenue IS NOT NULL
+ORDER BY
+  tr.total_revenue ASC
